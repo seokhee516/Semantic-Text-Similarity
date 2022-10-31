10c745fc773f45cc1ae80e0b849705024d4f4444
ask2DK <askDK@gmail.com>
2022-10-31 07:39:28 +0000

    test

--- train_new.py
+++ train_new.py
@@ -188,7 +188,7 @@ if __name__ == '__main__':
     # 터미널 실행 예시 : python3 run.py --batch_size=64 ...
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='klue/roberta-small', type=str)
+    parser.add_argument('--model_name', default='snunlp/KR-SBERT-V40K-klueNLI-augSTS', type=str)
     parser.add_argument('--batch_size', default=16, type=int)
     parser.add_argument('--max_epoch', default=10, type=int)
     parser.add_argument('--shuffle', default=True)
@@ -197,7 +197,7 @@ if __name__ == '__main__':
     parser.add_argument('--dev_path', default='./data/dev.csv')
     parser.add_argument('--test_path', default='./data/dev.csv')
     parser.add_argument('--predict_path', default='./data/test.csv')
-    parser.add_argument('--wandb_username', default='gwkim_22')
+    parser.add_argument('--wandb_username', default='dk100')
     parser.add_argument('--wandb_project', default='sts')
     parser.add_argument('--wandb_entity', default='sts_et')
 
